f1b467e6447932a0700a92c5b14b01c1516c37c8
Xepholin <marcweng6@hotmail.fr>
2023-12-20 09:42:41 +0100

    grid et solution analytique

--- src/lib_poisson1D.c
+++ src/lib_poisson1D.c
@@ -1,156 +1,155 @@
 /**********************************************/
 /* lib_poisson1D.c                            */
-/* Numerical library developed to solve 1D    */ 
+/* Numerical library developed to solve 1D    */
 /* Poisson problem (Heat equation)            */
 /**********************************************/
 // #include "lib_poisson1D.h"
 
+#include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <stddef.h>
 
-void set_GB_operator_colMajor_poisson1D(double* AB, int *lab, int *la, int *kv) {
+void set_GB_operator_colMajor_poisson1D(double *AB, int *lab, int *la, int *kv) {
 	int index = 0;
 
-	for (int i = 0; i < (*la); ++i)	{
+	for (int i = 0; i < (*la); ++i) {
+		if (i == 0) {
+			AB[index + (*kv)] = 0.0;
+			AB[index + (*kv) + 1] = 2.0;
+			AB[index + (*kv) + 2] = -1.0;
+		} else if (i == (*la) - 1) {
+			AB[index + (*kv)] = -1.0;
+			AB[index + (*kv) + 1] = 2.0;
+			AB[index + (*kv) + 2] = 0.0;
+		} else {
+			AB[index + (*kv)] = -1.0;
+			AB[index + (*kv) + 1] = 2.0;
+			AB[index + (*kv) + 2] = -1.0;
+		}
 
-    if (i == 0) {
-      AB[index + (*kv)] = 0.0;
-      AB[index + (*kv) + 1] = 2.0;
-      AB[index + (*kv) + 2] = -1.0;
-    }
-    else if (i == (*la)-1)  {
-      AB[index + (*kv)] = -1.0;
-      AB[index + (*kv) + 1] = 2.0;
-      AB[index + (*kv) + 2] = 0.0;
-    }
-    else  {
-      AB[index + (*kv)] = -1.0;
-      AB[index + (*kv) + 1] = 2.0;
-      AB[index + (*kv) + 2] = -1.0;
-    }
-
-    index += (*lab);
-  }
+		index += (*lab);
+	}
 }
 
-void set_GB_operator_colMajor_poisson1D_Id(double* AB, int *lab, int *la, int *kv)  {
-  int index = 0;
+void set_GB_operator_colMajor_poisson1D_Id(double *AB, int *lab, int *la, int *kv) {
+	int index = 0;
 
-	for (int i = 0; i < (*la); ++i)	{
+	for (int i = 0; i < (*la); ++i) {
+		AB[index + (*kv) + 1] = 1.0;
 
-    AB[index + (*kv) + 1] = 1.0; 
-
-    index += (*lab);
-  }
+		index += (*lab);
+	}
 }
 
-void set_dense_RHS_DBC_1D(double* RHS, int* la, double* BC0, double* BC1){
+void set_dense_RHS_DBC_1D(double *RHS, int *la, double *BC0, double *BC1) {
 	RHS[0] = *BC0;
 	RHS[*la - 1] = *BC1;
-}  
-
-void set_analytical_solution_DBC_1D(double* EX_SOL, double* X, int* la, double* BC0, double* BC1){
-}  
-
-void set_grid_points_1D(double* x, int* la){
 }
 
-void write_GB_operator_rowMajor_poisson1D(double* AB, int* lab, int* la, char* filename){
-  FILE * file;
-  int ii,jj;
-  file = fopen(filename, "w");
-  //Numbering from 1 to la
-  if (file != NULL){
-    for (ii=0;ii<(*lab);ii++){
-      for (jj=0;jj<(*la);jj++){
-	fprintf(file,"%lf\t",AB[ii*(*la)+jj]);
-      }
-      fprintf(file,"\n");
-    }
-    fclose(file);
-  }
-  else{
-    perror(filename);
-  }
+void set_analytical_solution_DBC_1D(double *EX_SOL, double *X, int *la, double *BC0, double *BC1) {
+	for (int i = 0; i < (*la); ++i)	{
+		EX_SOL[i] = (*BC0) + X[i] * ((*BC1) - (*BC0));
+	}
 }
 
-void write_GB_operator_colMajor_poisson1D(double* AB, int* lab, int* la, char* filename){
-  FILE * file;
-  int ii,jj;
-  file = fopen(filename, "w");
-  //Numbering from 1 to la
-  if (file != NULL){
-    for (ii=0;ii<(*la);ii++){
-      for (jj=0;jj<(*lab);jj++){
-	fprintf(file,"%lf\t",AB[ii*(*lab)+jj]);
-      }
-      fprintf(file,"\n");
-    }
-    fclose(file);
-  }
-  else{
-    perror(filename);
-  }
+void set_grid_points_1D(double *x, int *la) {
+	double h = 1 / ((*la) + 1);
+	x[0] = h;
+	for (int i = 1; i < (*la); ++i)	{
+		x[i] = i + h;
+	}
 }
 
-void write_GB2AIJ_operator_poisson1D(double* AB, int* la, char* filename){
-  FILE * file;
-  int jj;
-  file = fopen(filename, "w");
-  //Numbering from 1 to la
-  if (file != NULL){
-    for (jj=1;jj<(*la);jj++){
-      fprintf(file,"%d\t%d\t%lf\n",jj,jj+1,AB[(*la)+jj]);
-    }
-    for (jj=0;jj<(*la);jj++){
-      fprintf(file,"%d\t%d\t%lf\n",jj+1,jj+1,AB[2*(*la)+jj]);
-    }
-    for (jj=0;jj<(*la)-1;jj++){
-      fprintf(file,"%d\t%d\t%lf\n",jj+2,jj+1,AB[3*(*la)+jj]);
-    }
-    fclose(file);
-  }
-  else{
-    perror(filename);
-  }
+void write_GB_operator_rowMajor_poisson1D(double *AB, int *lab, int *la, char *filename) {
+	FILE *file;
+	int ii, jj;
+	file = fopen(filename, "w");
+	// Numbering from 1 to la
+	if (file != NULL) {
+		for (ii = 0; ii < (*lab); ii++) {
+			for (jj = 0; jj < (*la); jj++) {
+				fprintf(file, "%lf\t", AB[ii * (*la) + jj]);
+			}
+			fprintf(file, "\n");
+		}
+		fclose(file);
+	} else {
+		perror(filename);
+	}
 }
 
-void write_vec(double* vec, int* la, char* filename){
-  int jj;
-  FILE * file;
-  file = fopen(filename, "w");
-  // Numbering from 1 to la
-  if (file != NULL){
-    for (jj=0;jj<(*la);jj++){
-      fprintf(file,"%lf\n",vec[jj]);
-    }
-    fclose(file);
-  }
-  else{
-    perror(filename);
-  } 
-}  
-
-void write_xy(double* vec, double* x, int* la, char* filename){
-  int jj;
-  FILE * file;
-  file = fopen(filename, "w");
-  // Numbering from 1 to la
-  if (file != NULL){
-    for (jj=0;jj<(*la);jj++){
-      fprintf(file,"%lf\t%lf\n",x[jj],vec[jj]);
-    }
-    fclose(file);
-  }
-  else{
-    perror(filename);
-  } 
-}  
-
-int indexABCol(int i, int j, int *lab){
-  return 0;
+void write_GB_operator_colMajor_poisson1D(double *AB, int *lab, int *la, char *filename) {
+	FILE *file;
+	int ii, jj;
+	file = fopen(filename, "w");
+	// Numbering from 1 to la
+	if (file != NULL) {
+		for (ii = 0; ii < (*la); ii++) {
+			for (jj = 0; jj < (*lab); jj++) {
+				fprintf(file, "%lf\t", AB[ii * (*lab) + jj]);
+			}
+			fprintf(file, "\n");
+		}
+		fclose(file);
+	} else {
+		perror(filename);
+	}
 }
-int dgbtrftridiag(int *la, int*n, int *kl, int *ku, double *AB, int *lab, int *ipiv, int *info){
-  return *info;
+
+void write_GB2AIJ_operator_poisson1D(double *AB, int *la, char *filename) {
+	FILE *file;
+	int jj;
+	file = fopen(filename, "w");
+	// Numbering from 1 to la
+	if (file != NULL) {
+		for (jj = 1; jj < (*la); jj++) {
+			fprintf(file, "%d\t%d\t%lf\n", jj, jj + 1, AB[(*la) + jj]);
+		}
+		for (jj = 0; jj < (*la); jj++) {
+			fprintf(file, "%d\t%d\t%lf\n", jj + 1, jj + 1, AB[2 * (*la) + jj]);
+		}
+		for (jj = 0; jj < (*la) - 1; jj++) {
+			fprintf(file, "%d\t%d\t%lf\n", jj + 2, jj + 1, AB[3 * (*la) + jj]);
+		}
+		fclose(file);
+	} else {
+		perror(filename);
+	}
+}
+
+void write_vec(double *vec, int *la, char *filename) {
+	int jj;
+	FILE *file;
+	file = fopen(filename, "w");
+	// Numbering from 1 to la
+	if (file != NULL) {
+		for (jj = 0; jj < (*la); jj++) {
+			fprintf(file, "%lf\n", vec[jj]);
+		}
+		fclose(file);
+	} else {
+		perror(filename);
+	}
+}
+
+void write_xy(double *vec, double *x, int *la, char *filename) {
+	int jj;
+	FILE *file;
+	file = fopen(filename, "w");
+	// Numbering from 1 to la
+	if (file != NULL) {
+		for (jj = 0; jj < (*la); jj++) {
+			fprintf(file, "%lf\t%lf\n", x[jj], vec[jj]);
+		}
+		fclose(file);
+	} else {
+		perror(filename);
+	}
+}
+
+int indexABCol(int i, int j, int *lab) {
+	return 0;
+}
+int dgbtrftridiag(int *la, int *n, int *kl, int *ku, double *AB, int *lab, int *ipiv, int *info) {
+	return *info;
 }
--- src/tp_poisson1D_direct.c
+++ src/tp_poisson1D_direct.c
@@ -3,90 +3,92 @@
 /* This file contains the main function   */
 /* to solve the Poisson 1D problem        */
 /******************************************/
-#include "lib_poisson1D.h"
 #include "atlas_headers.h"
+#include "lib_poisson1D.h"
 
-int main(int argc,char *argv[])
+int main(int argc, char *argv[])
 /* ** argc: Nombre d'arguments */
 /* ** argv: Valeur des arguments */
 {
-  int ierr;
-  int jj;
-  int nbpoints, la;
-  int ku, kl, kv, lab;
-  int *ipiv;
-  int info;
-  int NRHS;
-  double T0, T1;
-  double *RHS, *EX_SOL, *X;
-  double **AAB;
-  double *AB;
+	int ierr;
+	int jj;
+	int nbpoints, la;
+	int ku, kl, kv, lab;
+	int *ipiv;
+	int info;
+	int NRHS;
+	double T0, T1;
+	double *RHS, *EX_SOL, *X;
+	double **AAB;
+	double *AB;
 
-  double temp, relres;
+	double temp, relres;
 
-  NRHS=1;
-  nbpoints=10;
-  la=nbpoints-2;
-  T0=-5.0;
-  T1=5.0;
+	NRHS = 1;
+	nbpoints = 10;
+	la = nbpoints - 2;
+	T0 = -5.0;
+	T1 = 5.0;
 
-  printf("--------- Poisson 1D ---------\n\n");
-  RHS=(double *) malloc(sizeof(double)*la);
-  EX_SOL=(double *) malloc(sizeof(double)*la);
-  X=(double *) malloc(sizeof(double)*la);
+	printf("--------- Poisson 1D ---------\n\n");
+	RHS = (double *)malloc(sizeof(double) * la);
+	EX_SOL = (double *)malloc(sizeof(double) * la);
+	X = (double *)malloc(sizeof(double) * la);
 
-  // TODO : you have to implement those functions
-  set_grid_points_1D(X, &la);
-  set_dense_RHS_DBC_1D(RHS,&la,&T0,&T1);
-  set_analytical_solution_DBC_1D(EX_SOL, X, &la, &T0, &T1);
-  
-  write_vec(RHS, &la, "RHS.dat");
-  write_vec(EX_SOL, &la, "EX_SOL.dat");
-  write_vec(X, &la, "X_grid.dat");
+	// TODO : you have to implement those functions
+	set_grid_points_1D(X, &la);
+	set_dense_RHS_DBC_1D(RHS, &la, &T0, &T1);
+	set_analytical_solution_DBC_1D(EX_SOL, X, &la, &T0, &T1);
 
-  kv=1;
-  ku=1;
-  kl=1;
-  lab=kv+kl+ku+1;
+	write_vec(RHS, &la, "RHS.dat");
+	write_vec(EX_SOL, &la, "EX_SOL.dat");
+	write_vec(X, &la, "X_grid.dat");
 
-  AB = (double *) malloc(sizeof(double)*lab*la);
+	kv = 1;
+	ku = 1;
+	kl = 1;
+	lab = kv + kl + ku + 1;
 
-  set_GB_operator_colMajor_poisson1D(AB, &lab, &la, &kv);
+	AB = (double *)malloc(sizeof(double) * lab * la);
 
-  write_GB_operator_colMajor_poisson1D(AB, &lab, &la, "AB.dat");
+	set_GB_operator_colMajor_poisson1D(AB, &lab, &la, &kv);
 
-  printf("Solution with LAPACK\n");
-  /* LU Factorization */
-  info=0;
-  ipiv = (int *) calloc(la, sizeof(int));
-  dgbtrf_(&la, &la, &kl, &ku, AB, &lab, ipiv, &info);
+	write_GB_operator_colMajor_poisson1D(AB, &lab, &la, "AB.dat");
 
-  /* LU for tridiagonal matrix  (can replace dgbtrf_) */
-  // ierr = dgbtrftridiag(&la, &la, &kl, &ku, AB, &lab, ipiv, &info);
+	printf("Solution with LAPACK\n");
+	/* LU Factorization */
+	info = 0;
+	ipiv = (int *)calloc(la, sizeof(int));
+	dgbtrf_(&la, &la, &kl, &ku, AB, &lab, ipiv, &info);
 
-  // write_GB_operator_colMajor_poisson1D(AB, &lab, &la, "LU.dat");
-  
-  /* Solution (Triangular) */
-  if (info==0){
-    // dgbtrs_("N", &la, &kl, &ku, &NRHS, AB, &lab, ipiv, RHS, &la, &info);
-    if (info!=0){printf("\n INFO DGBTRS = %d\n",info);}
-  }else{
-    printf("\n INFO = %d\n",info);
-  }
+	/* LU for tridiagonal matrix  (can replace dgbtrf_) */
+	// ierr = dgbtrftridiag(&la, &la, &kl, &ku, AB, &lab, ipiv, &info);
 
-  /* It can also be solved with dgbsv */
-  // TODO : use dgbsv
+	// write_GB_operator_colMajor_poisson1D(AB, &lab, &la, "LU.dat");
 
-  write_xy(RHS, X, &la, "SOL.dat");
+	/* Solution (Triangular) */
+	if (info == 0) {
+		// dgbtrs_("N", &la, &kl, &ku, &NRHS, AB, &lab, ipiv, RHS, &la, &info);
+		if (info != 0) {
+			printf("\n INFO DGBTRS = %d\n", info);
+		}
+	} else {
+		printf("\n INFO = %d\n", info);
+	}
 
-  /* Relative forward error */
-  // TODO : Compute relative norm of the residual
-  
-  printf("\nThe relative forward error is relres = %e\n",relres);
+	/* It can also be solved with dgbsv */
+	// TODO : use dgbsv
 
-  free(RHS);
-  free(EX_SOL);
-  free(X);
-  free(AB);
-  printf("\n\n--------- End -----------\n");
+	write_xy(RHS, X, &la, "SOL.dat");
+
+	/* Relative forward error */
+	// TODO : Compute relative norm of the residual
+
+	printf("\nThe relative forward error is relres = %e\n", relres);
+
+	free(RHS);
+	free(EX_SOL);
+	free(X);
+	free(AB);
+	printf("\n\n--------- End -----------\n");
 }
